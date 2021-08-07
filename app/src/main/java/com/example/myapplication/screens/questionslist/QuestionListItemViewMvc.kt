eedff439ce9d3caaa034c6dfdbe513228f136da8
Charlie <charlie@styletheory.co>
2021-08-07 15:40:07 +0700

    List Item MVC

---
+++ app/src/main/java/com/example/myapplication/screens/questionslist/QuestionListItemViewMvc.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication.screens.questionslist
+
+import android.view.View
+import com.example.myapplication.questions.Question
+
+interface QuestionListItemViewMvc {
+    interface Listener {
+        fun onQuestionClicked(question: Question)
+    }
+
+    fun getRootView(): View
+    fun registerListener(listener: Listener)
+    fun unregisterListener(listener: Listener)
+    fun bindQuestion(question: Question)
+}
---
+++ app/src/main/java/com/example/myapplication/screens/questionslist/QuestionListItemViewMvcImpl.kt
@@ -0,0 +1,48 @@
+package com.example.myapplication.screens.questionslist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.IdRes
+import com.example.myapplication.R
+import com.example.myapplication.questions.Question
+import com.example.myapplication.screens.questionslist.QuestionListItemViewMvc.Listener
+
+class QuestionListItemViewMvcImpl(inflater: LayoutInflater, parent: ViewGroup?): QuestionListItemViewMvc {
+
+    private val rootView: View
+    private val listeners: HashSet<Listener> = HashSet(1)
+    private lateinit var question: Question
+    private val txtTitle: TextView
+
+    init {
+        rootView = inflater.inflate(R.layout.question_list_item, parent, false)
+        txtTitle = findViewById(R.id.txt_title)
+        getRootView().setOnClickListener {
+            for (listener in listeners) {
+                listener.onQuestionClicked(question)
+            }
+        }
+
+
+        getRootView().setOnClickListener{ for (listener in listeners) }
+    }
+
+    private fun <T : View?> findViewById(@IdRes id: Int): T = getRootView().findViewById(id)
+
+    override fun getRootView(): View = rootView
+
+    override fun registerListener(listener: Listener) {
+        listeners.add(listener)
+    }
+
+    override fun unregisterListener(listener: Listener) {
+        listeners.remove(listener)
+    }
+
+    override fun bindQuestion(question: Question) {
+        this.question = question
+        txtTitle.text = question.title
+    }
+}
