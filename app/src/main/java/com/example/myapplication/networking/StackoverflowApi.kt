3f75a7ca077dec0e8769fa45768346846818214b
Charlie <charlie@styletheory.co>
2021-08-08 15:33:33 +0700

    add endpoint

--- app/src/main/java/com/example/myapplication/networking/StackoverflowApi.kt
+++ app/src/main/java/com/example/myapplication/networking/StackoverflowApi.kt
@@ -2,9 +2,13 @@ package com.example.myapplication.networking
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface StackoverflowApi {
     @GET("/questions?order=desc&sort=activity&site=stackoverflow")
     fun fetchLastActiveQuestions(@Query("pagesize") pageSize: Int?): Call<QuestionsListResponseSchema>
+
+    @GET("/questions/{questionId}?site=stackoverflow")
+    fun fetchQuestionDetails(@Path("questionId") questionId: String): Call<QuestionDetailsResponseSchema>
 }
